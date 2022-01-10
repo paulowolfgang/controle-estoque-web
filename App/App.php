884e5cecc23030c1e7e425f2440eaaa8364ab74c
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2022-01-09 22:13:35 -0300

    Classe Erro, Bootstrap add

--- App/App.php
+++ App/App.php
@@ -7,6 +7,11 @@
 
 	class App
 	{
+		private $controller;
+		private $controllerFile;
+		private $controllerName;
+		private $params;
+		private $action;
 
 		public function __construct()
 		{
---
+++ App/Libs/Erro.php
@@ -0,0 +1,31 @@
+<?php
+
+	namespace App\Libs;
+
+	use Exception;
+
+	class Erro
+	{
+		private $message;
+		private $code;
+
+		public function __construct($objetoException = Exception::class)
+		{
+			$this->message = $objetoException->getMessage();
+			$this->code    = $objetoException->getCode();
+		}
+
+		public function render()
+		{
+			$varMessage = $this->message;
+
+			if(file_exists(PATH . "/App/Views/error/" . $this->code . ".php"))
+			{
+				require_once PATH . "/App/Views/error/" .$this->code . ".php";
+			} else {
+				require_once PATH . "/App/Views/error/500.php";
+			}
+			exit;
+		}
+
+	}
