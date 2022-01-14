2450a16e4e0f501ee6c4faae28bb9bace5828c90
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2022-01-14 19:46:26 -0300

    Configuração de layout (menu)

--- App/Views/layouts/footer.php
+++ App/Views/layouts/footer.php
@@ -7,7 +7,9 @@
 	</footer>
 
 	<!-- ### JS Bootstrap ### -->
-    <script src="http://<?php echo APP_HOST; ?>/public/js/bootstrap.min.js"></script>
+    <script src="http://<?php echo APP_HOST; ?>/public/js/jquery/jquery-3.5.1.js"></script>
+	<!-- ### JS Bootstrap ### -->
+    <script src="http://<?php echo APP_HOST; ?>/public/js/bootstrap/bootstrap.min.js"></script>
 	
 	</body>
 </html>
--- App/Views/layouts/header.php
+++ App/Views/layouts/header.php
@@ -6,7 +6,7 @@
     	<meta name="viewport" content="width=device-width, initial-scale=1">
 
     	<!-- ### CSS Bootstrap ### -->
-    	<link href="http://<?php echo APP_HOST; ?>/public/css/bootstrap.min.css" rel="stylesheet">
+    	<link href="http://<?php echo APP_HOST; ?>/public/css/bootstrap/bootstrap.min.css" rel="stylesheet">
     	<!-- ### Font Awesome ### -->
     	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
