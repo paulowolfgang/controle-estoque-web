cbbc73befc025df08bd880707afbc1fa35dddb67
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2022-01-15 17:48:48 -0300

    Formulário de cadastro de usuário

--- App/Views/user/register.php
+++ App/Views/user/register.php
@@ -1,3 +1,44 @@
 <div class="container">
-     Página de cadastro de usuário.
+     <center>
+          <!-- ### Título da página ### -->
+          <div class="mt-5 mb-5">
+               <h2>
+                    Cadastro de Usuário
+               </h2>
+          </div>     
+     </center>
+     <!-- ### Início do formulário de cadastro -->
+     <div class="mb-5">
+          <form>
+               <div class="row col-lg-12 form-group">
+                    <label for="exampleInputEmail1"> Nome do usuário </label>
+                    <input type="email" class="form-control" id="nameUser" aria-describedby="emailHelp">
+               </div>
+               <div class="row col-lg-12 form-group">
+                    <label for="exampleInputEmail1"> E-mail do usuário </label>
+                    <input type="email" class="form-control" id="emailUser" aria-describedby="emailHelp">
+               </div>
+               <div class="row col-lg-12 form-group">
+                    <label for="exampleInputPassword1"> Senha do usuário </label>
+                    <input type="password" class="form-control" id="passUser">
+               </div>
+               <div class="row">
+                    <div class="col-lg-4 form-group">
+                         <label for="exampleFormControlSelect1">Tipo do usuário</label>
+                         <select class="form-control" id="typeUser">
+                              <option> Administrador </option>
+                              <option> Conferente </option>
+                         </select>
+                    </div>
+                    <div class="col-lg-4 form-group">
+                         <label for="exampleFormControlSelect1">Status do usuário</label>
+                         <select class="form-control" id="statusUser">
+                              <option>Ativado</option>
+                              <option>Desativado</option>
+                        </select>
+                    </div>
+               </div>
+               <button type="submit" class="btn btn-success">Cadastrar</button>
+          </form>
+     </div>
 </div>
