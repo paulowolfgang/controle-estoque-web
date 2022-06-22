a3439b6f4a59e9df078491cb922898eafa1381ac
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2022-06-22 10:28:54 -0300

    Criação do dashboard

--- App/Views/home/index.php
+++ App/Views/home/index.php
@@ -1,3 +1,37 @@
 <div class="container">
-     <h2 class="mt-5 mb-5"> Sistema de Controle de Estoque </h2>
+     <div class="row mt-5 mb-5">
+          <div class="col-md-4">
+               <div class="card text-center border-dark mb-3">
+                    <div class="card-header text-white bg-primary">
+                         Usuários
+                    </div>
+                    <div class="card-body text-dark">
+                         <h5 class="card-title">Total de cadastros</h5>
+                         <p class="card-text">08</p>
+                    </div>
+               </div>
+          </div>
+          <div class="col-md-4">
+               <div class="card text-center border-dark mb-3">
+                    <div class="card-header text-white bg-info">
+                         Fornecedores
+                    </div>
+                    <div class="card-body text-dark">
+                         <h5 class="card-title">Total de cadastros</h5>
+                         <p class="card-text">06</p>
+                    </div>
+               </div>
+          </div>
+          <div class="col-md-4">
+               <div class="card text-center border-dark mb-3">
+                    <div class="card-header text-white bg-success">
+                         Produtos
+                    </div>
+                    <div class="card-body text-dark">
+                         <h5 class="card-title">Total de cadastros</h5>
+                         <p class="card-text">45</p>
+                    </div>
+               </div>
+          </div>
+     </div>
 </div>
