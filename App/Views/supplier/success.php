67a0e2cef53248e80b56fadbc8766926ba6065d6
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2022-06-22 10:53:50 -0300

    Ajuste de redirecionamento após cadastro de entidades

--- App/Views/supplier/success.php
+++ App/Views/supplier/success.php
@@ -3,7 +3,7 @@
         <div class="col-sm-12 mt-5 mb-5">
             <h3>Tudo pronto, o cadastro foi realizado com sucesso.</h3>
             <br/>
-            <a href="http://<?php echo APP_HOST; ?>/supplier/register" class="btn btn-info">Voltar</a>
+            <a href="http://<?php echo APP_HOST; ?>/supplier/" class="btn btn-info">Voltar</a>
         </div>
     </div>
 </div>
--- App/Views/user/success.php
+++ App/Views/user/success.php
@@ -3,7 +3,7 @@
         <div class="col-sm-12 mt-5 mb-5">
             <h3>Tudo pronto, o cadastro foi realizado com sucesso.</h3>
             <br/>
-            <a href="http://<?php echo APP_HOST; ?>/user/register" class="btn btn-info mt-5">Voltar</a>
+            <a href="http://<?php echo APP_HOST; ?>/user/" class="btn btn-info mt-5">Voltar</a>
         </div>
     </div>
 </div>
